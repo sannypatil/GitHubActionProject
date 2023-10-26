141c5f3cc79531edef155775a3cd37b617f4420a
SPatil64 <santhosh.patil64@t-mobile.com>
2023-10-26 06:33:31 +0530

    Update Code

--- MyFirstGitHubActionWebApp/Program.cs
+++ MyFirstGitHubActionWebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dev API", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
