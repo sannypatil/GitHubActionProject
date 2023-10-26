c79aee7d05ada5e339d03ddca0c2c407c845be73
SPatil64 <santhosh.patil64@t-mobile.com>
2023-10-26 08:56:02 +0530

    Added Function

---
+++ MyHttpFunction/MyHttpFunction/Function1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace MyHttpFunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            return new ContentResult
+            {
+                Content = "<div style='text-align: center;'><h1>Hello, Santhosh. This HTTP triggered function executed successfully. </h1></div>",
+                ContentType = "text/html",
+                StatusCode = 200
+            };
+        }
+    }
+}
